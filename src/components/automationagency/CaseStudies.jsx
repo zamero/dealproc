67934d7e2ea77682a523e967a0825b690eb7badc
zamero <samer201001@gmail.com>
2024-10-21 21:55:44 +0200

    images

--- src/components/automationagency/CaseStudies.jsx
+++ src/components/automationagency/CaseStudies.jsx
@@ -1,75 +1,5 @@
-import { motion } from "framer-motion"
-
-import caseStudy1 from "../../assets/images/case1paragraph1.jpg"
-import caseStudy2 from "../../assets/images/feature2.jpg"
-import caseStudy3 from "../../assets/images/case1paragraph1image2.png"
-import caseStudy4 from "../../assets/images/InventoryManagement.png"
-import CloseMoreDeals from "../../assets/images/CloseMoreDeals.png"
-
-const caseStudiesData = {
-  "case-study-1": {
-    title: "AI-driven solution simplifies the entire inventory process",
-    description: "Snap a picture of an item, and the AI instantly identifies everything from material and color to its condition.",
-    image: caseStudy3,
-    section1: {
-      title: "Discover How AI Makes One Photo Unlock Instant, Detailed Inventory Insights",
-      content: "Our AI-driven solution simplifies the entire inventory process. Simply snap a picture of an item, and the AI instantly identifies and provides detailed information about it, everything from material and color to its condition. For example, take a photo of a chair, and the AI will tell you: `This is a swivel office chair, made of synthetic leather, in black, and in good condition`. Whether it's a chair, table, or even a flowerpot, our AI identifies the item and fills in the details, saving you hours of manual work.",
-      images: [caseStudy1, caseStudy3, caseStudy3, caseStudy3],
-    },
-    section2: {
-      title: "How Detailed Inventories Instantly Speed Up Property Sales",
-      content: "When selling a property, clarity is key. Providing potential buyers with a clear, detailed inventory of what's included builds trust and streamlines the process. Our AI-powered inventory solution helps sellers quickly and accurately document every item, giving buyers confidence while avoiding disputes later on. With automation handling the tedious work, sellers save time and ensure nothing is missed.",
-      images: [caseStudy4],
-    },
-    section3: {
-      title: "Close Deals Faster with Instant Inventory Reports",
-      content: "Buyers move quicker when they know exactly what they’re getting. By providing a detailed, automated inventory report early in the sales process, sellers eliminate uncertainty and speed up negotiations. Our system saves hours of manual work, ensuring every item is properly documented, so deals close faster.",
-      images: [CloseMoreDeals],
-    },
-  },
-  // "case-study-2": {
-  //   title: "Case Study 2",
-  //   description: "Description of case study 2",
-  //   image: caseStudy1,
-  //   section1: {
-  //     title: "Title 2",
-  //     content: "Content for section 2",
-  //     images: [caseStudy1, caseStudy2, caseStudy3, caseStudy1],
-  //   },
-  //   section2: {
-  //     title: "Title 2",
-  //     content: "Content for section 2",
-  //     images: [caseStudy2, caseStudy3],
-  //   },
-  //   section3: {
-  //     title: "Title 3",
-  //     content: "Content for section 3",
-  //     images: [caseStudy3, caseStudy1, caseStudy2, caseStudy3],
-  //   },
-  // },
-  // "case-study-3": {
-  //   title: "Case Study 3",
-  //   description: "Description of case study 3",
-  //   image: caseStudy1,
-  //   section1: {
-  //     title: "Title 3",
-  //     content: "Content for section 3",
-  //     images: [caseStudy1, caseStudy2, caseStudy3, caseStudy1],
-  //   },
-  //   section2: {
-  //     title: "Title 2",
-  //     content: "Content for section 2",
-  //     images: [caseStudy2, caseStudy3],
-  //   },
-  //   section3: {
-  //     title: "Title 3",
-  //     content: "Content for section 3",
-  //     images: [caseStudy3, caseStudy1, caseStudy2, caseStudy3],
-  //   },
-  // },
-};
-
-export { caseStudiesData };
+import { motion } from "framer-motion";
+import caseStudiesData from "./caseStudies.json";
 
 export const CaseStudies = () => (
   <section className="w-full flex justify-center pt-10 mt-16 mb-16 lg:mb-32 bg-customDarkBg2 relative">
@@ -89,22 +19,22 @@ export const CaseStudies = () => (
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-6 xl:px-0">
           {Object.entries(caseStudiesData).map(([key, caseStudy]) => (
-    <a
-      href={`/case-studies/${key}`}
-      className="custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col px-6 py-4 cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"
-      key={key}
-    >
-      <img src={caseStudy.image} alt={caseStudy.title} className="mb-4 rounded-lg" />
-      <div className="custom-content-text-white font-medium mb-2">
-        {caseStudy.title}
-      </div>
-      <div className="custom-content-text-gray">
-        {caseStudy.description}
-      </div>
-    </a>
-  ))}
+            <a
+              href={`/case-studies/${key}`}
+              className="custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col px-6 py-4 cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"
+              key={key}
+            >
+              <img src={caseStudy.image} alt={caseStudy.title} className="mb-4 rounded-lg" />
+              <div className="custom-content-text-white font-medium mb-2">
+                {caseStudy.title}
+              </div>
+              <div className="custom-content-text-gray">
+                {caseStudy.description}
+              </div>
+            </a>
+          ))}
         </div>
       </motion.div>
     </div>
   </section>
-)
+);
